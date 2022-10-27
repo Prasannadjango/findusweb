2e9f485b29aa8764a08bb9e0c9012a8c21e1bcaa
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2022-10-27 18:52:17 +0530

    first commit

---
+++ scripts/Swiper.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    breakpoints: {
+        // when window width is <= 499px
+        320: {
+            slidesPerView: 1,
+            spaceBetweenSlides: 50
+        },
+        // when window width is <= 999px
+        999: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 50
+        }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+  });
