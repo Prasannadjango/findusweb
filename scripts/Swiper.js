a72971d5f70f43ab9cb196316629daa70ef945c8
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2022-11-07 17:08:07 +0530

    changes

--- scripts/Swiper.js
+++ scripts/Swiper.js
@@ -34,6 +34,35 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+  var swiper = new Swiper(".Galleryswiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    breakpoints: {
+        // when window width is <= 499px
+        320: {
+            slidesPerView: 1,
+            spaceBetweenSlides: 50
+        },
+        768:{
+            slidesPerView: 2,
+            spaceBetweenSlides: 50
+        },
+        // when window width is <= 999px
+        999: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 50
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 50
+        }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+  });
+
   var swiper = new Swiper(".ExploreSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
